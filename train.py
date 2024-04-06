b34566571641ef9deeb1bbea0da485d50681ff2f
Tojo Tom <tojo@Tojos-MacBook-Air.local>
2024-04-06 15:42:51 +0530

    Simulating scenario 3 where both the actions pass

--- train.py
+++ train.py
@@ -10,7 +10,6 @@ y = df['Disease'].to_numpy()
 labels = np.sort(np.unique(y))
 y = np.array([np.where(labels == x) for x in y]).flatten()
 
-# model = LogisticRegression().fit(X, y)
 model = GaussianNB().fit(X, y)
 
 with open("model.pkl", 'wb') as f:
